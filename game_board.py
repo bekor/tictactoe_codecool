586526338de0b4b1eabcb1b065233be7dae2535b
KataBucsai <kata.kovacs@gmail.com>
2017-03-22 14:52:34 +0100

    input handler

--- game_board.py
+++ game_board.py
@@ -1,25 +1,28 @@
 import os
 
 
-def value_board_value(size):
+def value_board_value(size,value_b):
     for i in range(size):
-        value_board.append([" "] * size)
+        value_b.append([" "] * size)
 
 
-def print_board(size):
+def print_board(size, value_b, p_alpha):
     os.system('clear')
     print_first_row = []
     print_rows = []
     print("\n")
     for i in range(size):
-        print_first_row.append(" " + poss_alpha_inputs[i]+"  ")
+        print_first_row.append(" " + p_alpha[i]+"  ")
     print("   " + "".join(print_first_row))
     for i in range(size):
         for j in range(size):
             if j == 0:
-                print_rows.append("  " + value_board[i][j])                                
+                print_rows.append("  " + value_b[i][j])                                
             else:    
-                print_rows.append(" | " + value_board[i][j])
+                print_rows.append(" | " + value_b[i][j])
         print(" %s" % (i + 1) + "".join(print_rows))
         print_rows = []
-        print("   " + "-" * (size * 4 -1))
+        if i <= (size-2):
+            print("   " + "-" * (size * 4 -1))
+
+
---
+++ get_input.py
@@ -0,0 +1,45 @@
+def play_mode():
+    while True:
+        mode = input("Single Player: 1, Player vs Player: 2 : ")
+        if mode == "1":
+            return True
+        elif mode == "2":
+            return False
+        else:
+            print("You did not choose mode!")
+
+
+def get_board_size():
+    while True:
+        try:
+            size_input = int(input("Choose board size(3-9): "))
+            if size_input >= 3 and size_input <= 9:
+                return size_input
+            else:
+                print("Please choose valid size!")
+        except ValueError:
+            print("Please enter a number(3-9)!")
+
+
+def get_coordinate_input(p_alpha, p_num):
+    while True:
+        raw_coordinate = input("Insert coordinates (like: A1): ")
+        if (raw_coordinate[0] in p_alpha) and (raw_coordinate[1] in p_num):
+            coord = []
+            coord.append(int(raw_coordinate[1])-1)
+            coord.append(p_alpha.index(raw_coordinate[0]))
+            print(coord)
+            return coord
+        else:
+            print("Wrong row input")
+
+
+def replay_or_exit():
+    while True:
+        replay_or_exit = input("Do you want to play again(Y) or quit(Q): ")
+        if replay_or_exit.upper() == "Y":
+            return True
+        elif replay_or_exit.upper() == "Q":
+            exit()
+        else:
+            print("You have not decided!")
