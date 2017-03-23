ffa0449745449cbabd57a6153992066ee9e33736
matCsikai <ccsmatyas@gmail.com>
2017-03-23 13:50:37 +0100

    ai_ move and win_sequence_lenght functions definition

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -31,20 +31,27 @@ def player_move(sign):
         else:
             print("Wrong coordinates \n%s player try other coords" % sign)        
 
+def win_sequence_lenght(size):
+    if size > 3 and size < 8:
+        size = 4
+    elif size > 8:
+        size = 5
+    return size
 
 def game():
     ai = get_input.play_mode()
     size = get_input.get_board_size()
+    win_seq = win_sequence_lenght(size)
     board_value(size, BOARD)
     print_board(size, BOARD, poss_alpha_inputs)
     counter = 1
     win_cond = False
-    while counter < 10:
+    while counter < (size**2 + 1):
         
         if counter % 2 != 0:
             player_coord = player_move("x")
             print_board(size, BOARD, poss_alpha_inputs)
-            if check_win_contition(player_coord[0], player_coord[1], "x"):
+            if check_win_contition(player_coord[0], player_coord[1], "x", BOARD, size, win_seq):
                 print("X Player Win!")
                 break
 
@@ -52,26 +59,28 @@ def game():
             if counter % 2 == 0:
                 player_coord = player_move("o")
                 print_board(size, BOARD, poss_alpha_inputs)
-                if check_win_contition(player_coord[0], player_coord[1], "o"):
+                if check_win_contition(player_coord[0], player_coord[1], "o", BOARD, size, win_seq):
                     print("O Player Win!")
                     break
 
         else:
             if counter % 2 == 0:
-                ai_row = randint(0, 2)
-                ai_column = randint(0, 2)
-                ai_value = BOARD[ai_row][ai_column]
-                if ai_value == " ":
-                    BOARD[ai_row][ai_column] = "o"
-                    print_board(size, BOARD, poss_alpha_inputs)
-                    win_cond = check_win_contition(ai_row, ai_column, "o")
-                    if win_cond == True:
-                        print("AI Win!")
-                        break
-                else:
-                    counter -= 1
+                ai_coord = ai_move(size)
+                print_board(size, BOARD, poss_alpha_inputs)
+                if ai_coord:
+                    print("AI Win!")
         counter += 1
-         
+
+def ai_move(size):
+    while True:
+        ai_row = randint(0, size - 1)
+        ai_column = randint(0, size - 1)
+        ai_value = BOARD[ai_row][ai_column]
+        if ai_value == " ":
+            BOARD[ai_row][ai_column] = "o"
+            win_cond = check_win_contition(ai_row, ai_column, "o", BOARD, size, win_seq)
+            return win_cond
+   
 
 def main():
     while True:
